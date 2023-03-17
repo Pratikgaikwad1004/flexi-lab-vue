5f7a3a6c9b23759019d099922f74e9000b6367ac
Pratik Gaikwad <pratik@Pratiks-MacBook-Air.local>
2023-03-17 14:19:59 +0530

    Login form completed

---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="center">
+        <v-row>
+            <v-col cols="10" offset="1" sm="6" offset-sm="3" md="4" offset-md="4">
+                <v-avatar color="primary" size="128" rounded="1">
+                    <v-img src="@/assets/logo.png"></v-img>
+                </v-avatar>
+                <div style="margin-top: 20px;"></div>
+                <v-text-field v-model="username" filled label="Username" clearable></v-text-field>
+                <v-text-field v-model="password" filled label="Password" clearable type="password"></v-text-field>
+                <v-btn class="base-inverted" block @click="onLogin()">
+                    Login
+                </v-btn>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: "",
+            password: ""
+        }
+    },
+    methods: {
+        onLogin() {
+            alert("Clicked")
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Login Page</h1>
+        vrow
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LoginPage",
+        data() { }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
