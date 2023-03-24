54684967b34878672427c2b839595425783788e6
Pratik Gaikwad <pratik@Pratiks-MacBook-Air.local>
2023-03-24 14:21:39 +0530

    books

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -2,7 +2,7 @@
     <div>
         <v-list dense>
             <v-list-item v-for="(item, i) in books" :key="i">
-                <v-list-item-avatar>
+                <v-list-item-avatar rounded="0">
                     <v-img :src="item.img"></v-img>
                 </v-list-item-avatar>
 
@@ -13,9 +13,34 @@
                 </v-list-item-content>
 
                 <v-list-item-icon>
-                    <v-icon>
+                    <div>
+                        <v-menu offset-y>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn color="black" dark v-bind="attrs" v-on="on" text>
+                                    <v-icon>mdi-dots-vertical</v-icon>
+                                </v-btn>
+                            </template>
+                            <v-list>
+                                <v-list-item>
+                                    <v-icon class="mr-5">mdi-pencil</v-icon>
+                                    <v-list-item-title>Edit</v-list-item-title>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-icon class="mr-5">mdi-delete</v-icon>
+                                    <v-list-item-title>Delete</v-list-item-title>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-list-item-title>Issue</v-list-item-title>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-list-item-title>Return</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                        </v-menu>
+                    </div>
+                    <p>
                         status
-                    </v-icon>
+                    </p>
                 </v-list-item-icon>
             </v-list-item>
         </v-list>
